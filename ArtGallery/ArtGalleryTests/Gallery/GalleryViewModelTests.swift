228b46f9a8ee6e892653979d411ef64e7cdb6200
Kim Egenvall <egenvall.development@gmail.com>
2021-05-23 20:41:48 +0200

    Add Failing GalleryViewModel test

--- ArtGallery/ArtGalleryTests/Gallery/GalleryViewModelTests.swift
+++ ArtGallery/ArtGalleryTests/Gallery/GalleryViewModelTests.swift
@@ -7,42 +7,42 @@ import XCTest
 
 class ListViewModelTests: XCTestCase {
     private var viewModel: GalleryViewModel!
-    private let assetOne = ArtAsset(id: "id", title: "title", artist: "artist", imageAsset: ArtAssetImage(imageUrl: "imageUrl", width: 1, height: 1))
-        
-    private let assetTwo = ArtAsset(id: "id2", title: "title2", artist: "artist2", imageAsset: ArtAssetImage(imageUrl: "imageUrl2", width: 1, height: 1))
-    
+    private let responseObject = ArtAsset(id: "id", title: "title", artist: "artist", imageAsset: ArtAssetImage(imageUrl: "imageUrl", width: 1, height: 1))
+    private let responseObjectTwo = ArtAsset(id: "id2", title: "title2", artist: "artist2", imageAsset: ArtAssetImage(imageUrl: "imageUrl2", width: 1, height: 1))
     private var disposables: Set<AnyCancellable>!
+    
     override func setUpWithError() throws {
-        //viewModel = GalleryViewModel(Mock)
+        viewModel = GalleryViewModel(MockGalleryViewModelDependency([responseObject]))
         disposables = []
     }
     
     func testSearch() {
-//        let expectedResult = ArtViewModel(id: asset.id, title: asset.title, artist: asset.artist, imageAsset: RemoteImageViewModel(imageUrl: asset.imageAsset.imageUrl, width: asset.imageAsset.width, height: asset.imageAsset.height))
-//        viewModel.subscribe()
-//        viewModel.searchInput.send("Test")
-//        let expectation = XCTestExpectation(description: "Search")
-//        viewModel.$items.sink(receiveValue: { value in
-//            if value == [expectedResult] {
-//                expectation.fulfill()
-//            }
-//        }).store(in: &disposables)
-//        wait(for: [expectation], timeout: 1)
+        let expectedResult = ArtViewModel(id: responseObject.id, title: responseObject.title, artist: responseObject.artist, imageAsset: RemoteImageViewModel(imageUrl: responseObject.imageAsset.imageUrl, width: responseObject.imageAsset.width, height: responseObject.imageAsset.height))
+        viewModel.subscribe()
+        viewModel.searchInput.send("Test")
+        let expectation = XCTestExpectation(description: "Search")
+        viewModel.$items.sink(receiveValue: { value in
+            if value == [expectedResult] {
+                expectation.fulfill()
+            }
+        }).store(in: &disposables)
+        wait(for: [expectation], timeout: 1)
     }
     func testPagination() {
-//        let expectedResult = ArtViewModel(id: asset.id, title: asset.title, artist: asset.artist, imageAsset: RemoteImageViewModel(imageUrl: asset.imageAsset.imageUrl, width: asset.imageAsset.width, height: asset.imageAsset.height))
-//        viewModel.items = [expectedResult]
-//        viewModel.prefetchIfNeeded(0)
-//        let expectation = XCTestExpectation(description: "Pagination")
-//        viewModel.$items.sink(receiveValue: { value in
-//            if value.count == 2 {
-//                if value == [expectedResult, expectedResult] {
-//                    expectation.fulfill()
-//                }
-//            }
-//            
-//        }).store(in: &disposables)
-//        wait(for: [expectation], timeout: 1)
+        viewModel = GalleryViewModel(MockGalleryViewModelDependency([responseObjectTwo]))
+        let responseObjectOneViewModel = ArtViewModel(id: responseObject.id, title: responseObject.title, artist: responseObject.artist, imageAsset: RemoteImageViewModel(imageUrl: responseObject.imageAsset.imageUrl, width: responseObject.imageAsset.width, height: responseObject.imageAsset.height))
+        let responseObjectTwoViewModel = ArtViewModel(id: responseObjectTwo.id, title: responseObjectTwo.title, artist: responseObjectTwo.artist, imageAsset: RemoteImageViewModel(imageUrl: responseObjectTwo.imageAsset.imageUrl, width: responseObjectTwo.imageAsset.width, height: responseObjectTwo.imageAsset.height))
+        let expectedResult = [responseObjectOneViewModel, responseObjectTwoViewModel]
+        viewModel.items = [responseObjectOneViewModel]
+        viewModel.prefetchIfNeeded(0)
+        let expectation = XCTestExpectation(description: "Pagination")
+        viewModel.$items.sink(receiveValue: { value in
+            if value == expectedResult {
+                expectation.fulfill()
+            }
+
+        }).store(in: &disposables)
+        wait(for: [expectation], timeout: 1)
     }
 }
 
--- ArtGallery/ArtGalleryTests/Gallery/MockGalleryViewModelDependency.swift
+++ ArtGallery/ArtGalleryTests/Gallery/MockGalleryViewModelDependency.swift
@@ -1,14 +1,18 @@
 import NetworkingModule
 import Combine
 import Foundation
-final class MockDependency: ArtResolver {
+final class MockGalleryViewModelDependency: ArtResolver {
     private let assetResponse: [ArtAsset]
     init(_ responseObject: [ArtAsset]) {
         self.assetResponse = responseObject
     }
-    func addParameter(_ param: QueryParameter) -> IQueryBuilder {}
+    func addParameter(_ param: QueryParameter) -> IQueryBuilder {
+        fatalError()
+    }
     
-    func build() -> URL? {}
+    func build() -> URL? {
+        return nil
+    }
     
     func search(_ text: String) -> AnyPublisher<[IArtAsset], EndpointError> {
         Just(assetResponse).setFailureType(to: EndpointError.self).eraseToAnyPublisher()
