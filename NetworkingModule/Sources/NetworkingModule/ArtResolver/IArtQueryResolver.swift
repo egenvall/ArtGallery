20e466738ea43a07ace628deaa3d1561b9cc8e5a
Kim Egenvall <egenvall.development@gmail.com>
2021-05-23 18:58:59 +0200

    Refactor model name

--- NetworkingModule/Sources/NetworkingModule/ArtResolver/IArtQueryResolver.swift
+++ NetworkingModule/Sources/NetworkingModule/ArtResolver/IArtQueryResolver.swift
@@ -1,5 +1,5 @@
 import Combine
 public protocol IArtQueryResolver {
-    func search(_ text: String) -> AnyPublisher<[IAssetData], EndpointError>
-    func paginate() -> AnyPublisher<[IAssetData], EndpointError>
+    func search(_ text: String) -> AnyPublisher<[IArtAsset], EndpointError>
+    func paginate() -> AnyPublisher<[IArtAsset], EndpointError>
 }
