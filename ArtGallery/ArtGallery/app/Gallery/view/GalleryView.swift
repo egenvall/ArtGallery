10a281421a0f3c9570eeaf179b54ec5e1b65a6c0
Kim Egenvall <egenvall.development@gmail.com>
2021-05-23 20:55:05 +0200

    Remove placeholder comment

--- ArtGallery/ArtGallery/app/Gallery/view/GalleryView.swift
+++ ArtGallery/ArtGallery/app/Gallery/view/GalleryView.swift
@@ -2,11 +2,6 @@ import SwiftUI
 struct GalleryView<ViewModel: IGalleryViewModel>: View {
     @ObservedObject var viewModel: ViewModel
     var body: some View {
-        /*
-         Z
-            Search
-            Grid
-         */
         Color.blue
     }
 }
